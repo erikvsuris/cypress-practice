41138b36c635d47e8e5c8f529fced011037b27f7
erikvsuris <erikvsuris@gmail.com>
2025-04-15 15:37:22 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+  it('Gets, types and asserts', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
+
+    cy.get('.action-email').type('fake@email.com')
+
+    cy.get('.action-email').should('have.value', 'fake@email.com')
   })
 })
